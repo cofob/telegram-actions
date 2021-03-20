1b85a9cf5fc3a207b3221dd2d000eb4181b50b27
cofob <cofob@tutanota.com>
2021-03-21 03:50:11 +0700

    1

--- run.py
+++ run.py
@@ -26,41 +26,41 @@ response = f"Hello there, \n"
 # Process the event and prepare Whatsapp message payload
 media_url_for_avatar = [f"{GITHUB_SERVER_URL}/{GITHUB_ACTOR}.png"]
 if GITHUB_EVENT_NAME == "push":
-    response += f"There is a new *push* in your repository *{REPOSITORY}* by *{GITHUB_ACTOR}*.\n\n"
-    response += f"*Repository URL*: {repo_url}"
+    response += f"There is a new <b>push</b> in your repository <b>{REPOSITORY}</b> by <b>{GITHUB_ACTOR}</b>.\n\n"
+    response += f"<b>Repository URL</b>: {repo_url}"
 elif GITHUB_EVENT_NAME == "pull_request":
     pr_url = f"{repo_url}/pull/{PR_NUMBER}"
-    response += f"A new event was triggered for a *Pull Request* in your repository *{REPOSITORY}*.\n\n"
-    response += f"*PR Number*: ```#{PR_NUMBER}```\n"
-    response += f"*PR Title* : ```{PR_TITLE}```\n"
-    response += f"*PR User*  : ```{GITHUB_ACTOR}```\n"
-    response += f"*PR Body*  : _{PR_BODY}_\n\n"
-    response += f"*Check it out*: {pr_url}\n"
-    response += f"*Repository URL*: {repo_url}"
+    response += f"A new event was triggered for a <b>Pull Request</b> in your repository <b>{REPOSITORY}</b>.\n\n"
+    response += f"<b>PR Number</b>: ```#{PR_NUMBER}```\n"
+    response += f"<b>PR Title</b> : ```{PR_TITLE}```\n"
+    response += f"<b>PR User</b>  : ```{GITHUB_ACTOR}```\n"
+    response += f"<b>PR Body</b>  : _{PR_BODY}_\n\n"
+    response += f"<b>Check it out</b>: {pr_url}\n"
+    response += f"<b>Repository URL</b>: {repo_url}"
 elif GITHUB_EVENT_NAME == "issues":
     issue_url = f"{repo_url}/issues/{ISSUE_NUMBER}"
-    response += f"A new event was triggered for an *Issue* in your repository *{REPOSITORY}*.\n\n"
-    response += f"*Issue Number*: ```#{ISSUE_NUMBER}```\n"
-    response += f"*Issue Title* : ```{ISSUE_TITLE}```\n"
-    response += f"*Issue User*  : ```{GITHUB_ACTOR}```\n"
-    response += f"*Issue Body*  : _{ISSUE_BODY}_\n\n"
-    response += f"*Check it out*: {issue_url}\n"
-    response += f"*Repository URL*: {repo_url}"
+    response += f"A new event was triggered for an <b>Issue</b> in your repository <b>{REPOSITORY}</b>.\n\n"
+    response += f"<b>Issue Number</b>: ```#{ISSUE_NUMBER}```\n"
+    response += f"<b>Issue Title</b> : ```{ISSUE_TITLE}```\n"
+    response += f"<b>Issue User</b>  : ```{GITHUB_ACTOR}```\n"
+    response += f"<b>Issue Body</b>  : _{ISSUE_BODY}_\n\n"
+    response += f"<b>Check it out</b>: {issue_url}\n"
+    response += f"<b>Repository URL</b>: {repo_url}"
 elif GITHUB_EVENT_NAME == "fork":
-    response += f"Your repository *{REPOSITORY}* was forked by *{GITHUB_ACTOR}*.\n"
-    response += f"Current fork count: *{REPO_FORK_COUNT}*\n\n"
-    response += f"*Repository URL*: {repo_url}"
+    response += f"Your repository <b>{REPOSITORY}</b> was forked by <b>{GITHUB_ACTOR}</b>.\n"
+    response += f"Current fork count: <b>{REPO_FORK_COUNT}</b>\n\n"
+    response += f"__Repository URL__: {repo_url}"
 elif GITHUB_EVENT_NAME == "watch":
-    response += f"Your repository *{REPOSITORY}* is now watched by *{GITHUB_ACTOR}*.\n"
-    response += f"Current watch count: *{REPO_WATCH_COUNT}*\n\n"
-    response += f"*Repository URL*: {repo_url}"
+    response += f"Your repository <b>{REPOSITORY}</b> is now watched by <b>{GITHUB_ACTOR}</b>.\n"
+    response += f"Current watch count: <b>{REPO_WATCH_COUNT}</b>\n\n"
+    response += f"__Repository URL__: {repo_url}"
 else:
-    response += f"A new *{GITHUB_EVENT_NAME}* event was triggered by *{GITHUB_ACTOR}* in your repository *{REPOSITORY}*\n\n"
-    response += f"*Repository URL*: {repo_url}"
+    response += f"A new <b>{GITHUB_EVENT_NAME}</b> event was triggered by <b>{GITHUB_ACTOR}</b> in your repository <b>{REPOSITORY}</b>\n\n"
+    response += f"<b>Repository URL</b>: {repo_url}"
 
 # Prepare and send the message payload
 media_url = media_url_for_avatar if media_url_for_avatar else None,
 body = response
 
-r = requests.get(f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={CHAT_ID}&text={response}')
+r = requests.get(f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={CHAT_ID}&text={response}&parse_mode=html')
 print('status -> '+str(r.status_code))
